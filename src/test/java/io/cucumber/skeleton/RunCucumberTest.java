d9456187997d261fbd37656190d4c05ae243be19
Mathew Davy <davym@assurity.local>
2021-11-19 12:57:57 +1300

    trying aggregate report plugin

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -1,10 +1,33 @@
 package io.cucumber.skeleton;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:build/cucumber-reports/cucumber-report.json"})
+@RunWith(ExtendedCucumber.class)
+
+@ExtendedCucumberOptions(
+         jsonReport = "build/reports/json/testresult.json",
+        customTemplatesPath = "/templates/reports/templates.json",
+        overviewReport = true,
+        jsonUsageReport = "build/reports/json/cucumber-usage.json",
+        detailedAggregatedReport = true,
+        toPDF = true,
+        outputFolder = "build/reports/cucumber_reports/"
+)
+@CucumberOptions(
+        plugin = {
+                "html:build/reports/html",
+                "json:build/reports/json/testresult.json",
+                "pretty:build/reports/cucumber-pretty.txt",
+                "usage:build/reports/json/cucumber-usage.json",
+                "junit:build/reports/cucumber-results.xml",
+        },
+        features = "src/test/resources/io/cucumber/skeleton",
+        glue = "src/test/java/io/cucumber/skeleton"
+
+)
+
 public class RunCucumberTest {
 }
--- src/test/resources/io/cucumber/skeleton/belly.feature
+++ src/test/resources/io/cucumber/skeleton/belly.feature
@@ -1,4 +1,4 @@
-Feature: Belly
+Feature: Belly @tag
 
   Scenario: a few cukes
     Given I have 42 cukes in my belly
