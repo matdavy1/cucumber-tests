9d287f931b0420c60210deb74ffdd8d78eb7c6ab
Mat Davy <mathew.davy@ird.govt.nz>
2021-11-23 17:09:30 +1300

    logging response

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -5,10 +5,20 @@ import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import cucumber.api.Scenario;
 import cucumber.api.java.Before;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.junit.Assert.*;
 
 public class StepDefinitions {
-    public Scenario scenario;
+    private Scenario scenario;
+    private HttpURLConnection con;
 
     @Before
     public void setupScenario(Scenario scenario){
@@ -19,7 +29,6 @@ public class StepDefinitions {
     public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
-        scenario.write("Hi");
     }
 
     @When("^I wait (.+) hour$")
@@ -36,5 +45,33 @@ public class StepDefinitions {
         Belly belly = new Belly();
         belly.growl();
     }
+
+    @Given("^I make a request to a public api$")
+    public void I_make_a_request_to_a_public_api() throws IOException {
+        URL url = new URL("https://jsonplaceholder.typicode.com/todos/1");
+        con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+
+        scenario.write(con.getURL().toString());
+    }
+
+    @Then("^I should get a 200 response$")
+    public void I_should_get_a_200_response() throws IOException {
+        assertEquals(200, con.getResponseCode());
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        con.disconnect();
+        scenario.write(content.toString());
+    }
+
 
 }
---
+++ src/test/resources/io/cucumber/skeleton/api.feature
@@ -0,0 +1,5 @@
+Feature: Api
+
+  Scenario: make an api call
+    Given I make a request to a public api
+    Then I should get a 200 response
